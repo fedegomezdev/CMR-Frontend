b209978c557abc5968e28aacbe3447b974856fad
fede <fedegomezdev@gmail.com>
2019-10-17 18:55:25 -0300

    empezando la parte de clientes 323

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React,{useState, useEffect, Fragment} from 'react';
+
+import clienteAxios from '../../config/axios';
+
+import Cliente from './Cliente';
 
 function Clientes(){
-    return(
-        
-        <h1>Clientes</h1>
 
+    //State
+    const [clientes, guardarClientes] = useState([]);
+
+    //Query a la api
+    const consultarApi = async() => {
+        const clientesConsulta = await clienteAxios.get('/clientes');
+
+        //colocar en el state
+        guardarClientes(clientesConsulta.data);
+    }
+
+    useEffect( () =>{
+
+        consultarApi();
+    }, [] );
+
+    return(
+        <Fragment>
+            <h2>Clientes</h2>
+
+            <ul className="listado-clientes">
+                {clientes.map(cliente => { {/* podria no poner el corchete  y return y poner directo "(""  */ }
+                    return(
+                    <Cliente 
+                        key={cliente._id}
+                        cliente={cliente}
+                    />
+                    )
+                })}
+            </ul>
+        </Fragment>
     )
 }
 
